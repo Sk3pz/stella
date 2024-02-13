f7e5ac334cdd2ddce05545cf5ea7af88d41e9c71
skepz <skepz.dev@gmail.com>
2024-02-13 13:19:17 -0600

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use bevy::app::{App, Update};
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+fn main() {
+    App::new()
+        .add_systems(Update, hello_world)
+        .run();
 }
